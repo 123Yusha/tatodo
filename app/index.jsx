70cb4ec8de032abc2c0286a306316538998c6ac9
123Yusha <yr130488@googlemail.com>
2025-02-14 16:17:48 +0000

    styled button post account deletion

--- app/index.jsx
+++ app/index.jsx
@@ -13,9 +13,9 @@ export default function Index() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text> Welcome! </Text>
-      <TouchableOpacity onPress={handleBrowse}>
-        <Text>Browse App</Text>
+      <Text style={styles.headerText}> You are no longer signed in! </Text>
+      <TouchableOpacity style={styles.button} onPress={handleBrowse}>
+        <Text style={styles.buttonText}>Browse App</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
@@ -27,5 +27,32 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff',
+  },
+  headerText: {
+    fontSize: 24,
+    fontFamily: "outfit-bold",
+    color: "#171616",
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#171616",
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 18,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+    width: "80%",
+    elevation: 5,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#fff",
+    fontFamily: "outfit-regular",
   },
 });
